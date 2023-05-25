2ff887a6a909a451eefe4be522dc5f7f0c6fc7e3
F.HODAKA <hodamaru0523@yahoo.co.jp>
2023-05-25 15:06:44 +0900

    [Add]Itemの叫び声を追加しました。          完成したItemをプレハブ化しました。 [Remove]必要のない画像を削除しました。

--- Assets/Fukushima/Script/TestItem.cs
+++ Assets/Fukushima/Script/TestItem.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TestItem : MonoBehaviour
 {
+    public AudioClip _scream;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +17,21 @@ public class TestItem : MonoBehaviour
 
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("“–‚½‚Á‚Ä‚é");
-        if (other.gameObject.name == "TestPlayer")
+        //"@"“à‚ğplayer‚Ìobject–¼‚É•ÏX‚µ‚Ä‚­‚¾‚³‚¢ 
+        if (collision.gameObject.name == "TestPlayer")
         {
             Debug.Log("TestPlayer‚É“–‚½‚Á‚Ä‚é");
-            Iitem _item = other.gameObject.GetComponent<Iitem>();
+            Iitem _item = collision.gameObject.GetComponent<Iitem>();
             if (_item != null)
             {
                 _item.AddHP(5);
                 Debug.Log("HP + 5");
+                AudioSource.PlayClipAtPoint(_scream,this.transform.position);
                 Destroy(this.gameObject);
             }
-
         }
     }
 }

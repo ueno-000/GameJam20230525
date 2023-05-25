c8c5b4abec88706fbb3fb79e5e4a0204ffc36205
F.HODAKA <hodamaru0523@yahoo.co.jp>
2023-05-25 15:34:17 +0900

    [Add]BGMを追加しました。 [Update]Itemの叫び声を短めのものに変更しました。

--- Assets/Fukushima/Script/TestItem.cs
+++ Assets/Fukushima/Script/TestItem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TestItem : MonoBehaviour
 {
     public AudioClip _scream;
+    public AudioClip _eat;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +28,7 @@ public class TestItem : MonoBehaviour
             Iitem _item = collision.gameObject.GetComponent<Iitem>();
             if (_item != null)
             {
+                AudioSource.PlayClipAtPoint(_eat,this.transform.position);
                 _item.AddHP(5);
                 Debug.Log("HP + 5");
                 AudioSource.PlayClipAtPoint(_scream,this.transform.position);
